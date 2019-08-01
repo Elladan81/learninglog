d841ee26f26d6e75a2024b71ec34ae1bdd1bbe65
Micka <35881360+Elladan81@users.noreply.github.com>
2019-08-01 19:32:05 +0200

    create a "add new entry forms and template"

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -12,7 +12,7 @@ class Topic(models.Model):
 
 
 class Entry(models.Model):
-    """someting specific learn about a topic"""
+    """something specific learn about a topic"""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
